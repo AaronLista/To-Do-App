1a7f9e4a19634dcc67f1a5c1ccd3c460e6d6e9e7
AaronLista <aarondavidlista@gmail.com>
2023-11-02 09:21:59 -0400

    se deshabilita el botton de agregar tareas mientras la app carga

--- src/components/DisplayModalBtn/DisplayModalBtn.js
+++ src/components/DisplayModalBtn/DisplayModalBtn.js
@@ -3,9 +3,9 @@ import { TodoContext } from "../Context/TodoContext";
 import './DisplayModalBtn.css'
 
 function DisplayModalBtn(){
-        const {toggleModal, setToggleModal} = React.useContext(TodoContext)
+        const {toggleModal, setToggleModal, loading} = React.useContext(TodoContext)
     return(
-        <button
+        <button disabled={loading}
             className="displayBtn"
             onClick={()=>{setToggleModal(!toggleModal)}}
         >
